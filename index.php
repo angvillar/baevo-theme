f5fe7abfce10d6be2b2d36414216df09f17189dc
angvillar <angvillar@gmail.com>
2018-05-25 14:04:40 +0200

    section services in home page

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 $context = Timber::get_context();
 $context['section_cover'] = get_field('section_cover');
+$context['section_services'] = get_field('section_services');
 $context['section_portfolio'] = get_field('section_portfolio');
 $context['portfolio'] = Timber::get_posts(array(
   'post_type' => 'portfolio',
